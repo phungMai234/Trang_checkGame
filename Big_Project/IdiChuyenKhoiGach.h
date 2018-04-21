cff92254ee3113db8ee85b4a3daeffaf40e76b5e
Your Namemai <phungtuyet234@gmail.com>
2018-04-21 20:58:02 +0700

    2.0_mai

---
+++ Big_Project/IdiChuyenKhoiGach.h
@@ -0,0 +1,14 @@
+#ifndef IDICHUYENKHOIGACH_H_INCLUDED
+#define IDICHUYENKHOIGACH_H_INCLUDED
+
+bool Inside(int, int);
+bool Left(int, int);
+bool Right(int, int);
+bool Down(int,int);
+
+int RoiXuong(KhoiGach*);
+void SangPhai(KhoiGach*);
+void SangTrai(KhoiGach*);
+void XoayKhoiGach(KhoiGach*);
+
+#endif // IDICHUYENKHOIGACH_H_INCLUDED
--- Big_Project/IstructKhoiGach.h
+++ Big_Project/IstructKhoiGach.h
@@ -1,6 +1,11 @@
 #ifndef ISTRUCTKHOIGACH_H_INCLUDED
 #define ISTRUCTKHOIGACH_H_INCLUDED
 
+#define LEFT 4
+#define TOP 4
+
+#define MaxI 22
+#define MaxJ 10
 struct KhoiGach
 {
     int **arr; // la cai j
