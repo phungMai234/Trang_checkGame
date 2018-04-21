7787982b191379d2f2acd32954090d281d763cb1
Your Namemai <phungtuyet234@gmail.com>
2018-04-21 20:40:03 +0700

    1.0_mai

---
+++ Big_Project/IstructKhoiGach.h
@@ -0,0 +1,11 @@
+#ifndef ISTRUCTKHOIGACH_H_INCLUDED
+#define ISTRUCTKHOIGACH_H_INCLUDED
+
+struct KhoiGach
+{
+    int **arr; // la cai j
+    int Row, Col;
+    int iBoard, jBoard; // jBoard chỉ h� ng, iBoard chỉ cột
+};
+
+#endif // ISTRUCTKHOIGACH_H_INCLUDED
---
+++ Big_Project/structKhoiGach.cpp
@@ -0,0 +1,3 @@
+
+#include"IstructKhoiGach.h"
+
